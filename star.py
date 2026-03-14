e221db00edb1056bf194c9fd87692a2b53e144da
Ramisha <rameeesha23@gmail.com>
2026-03-15 00:03:54 +0500

    three

---
+++ star.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+go = turtle.Turtle()
+ 
+go.forward(100)
+go.left(120)
+go.forward(100)
+ 
+go.left(120)
+go.forward(100)
+go.penup()
+go.right(150)
+go.forward(50)
+ 
+go.pendown()
+go.right(90)
+go.forward(100)
+go.right(120)
+go.forward(100)
+ 
+go.right(120)
+go.forward(100)
+ 
+turtle.done()
