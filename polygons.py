c9925666b993562b2ef9c1436eaaf3f1f70d01da
Ramisha <rameeesha23@gmail.com>
2026-03-15 00:01:41 +0500

    two

---
+++ polygons.py
@@ -0,0 +1,20 @@
+import turtle
+turtle.Screen().bgcolor("yellow")
+go = turtle.Turtle()
+ 
+# Triangle
+go.forward(100)
+go.left(120)
+go.forward(100)
+go.left(120)
+go.forward(100)
+
+# Square
+go.forward(100)
+go.left(90)
+go.forward(100)
+go.left(90)
+go.forward(100)
+go.left(90)
+go.forward(100)
+go.left(90)
